66c3b5f88b7cd4115ef102140366fc41ea6f9a2a
Pigchick <pigchick1623@gmail.com>
2026-01-31 19:19:06 +0100

    Fix animate

--- Assets/Scripts/Actors/BlueActor.cs
+++ Assets/Scripts/Actors/BlueActor.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace GGJ2026
@@ -13,5 +14,21 @@ namespace GGJ2026
         }
 
         public override FactionColor CombatColor => FactionColor.Blue;
+
+        [SerializeField] private Transform visual; // 建议是子物体
+        [SerializeField] private float baseMoveTime = 0.2f;
+        public override float PlayMoveAnimation(Vector3 from, Vector3 to, int tiles)
+        {
+            float duration = baseMoveTime * Mathf.Max(1, tiles);
+            transform.position = from;
+            transform.DOMove(to, duration).SetEase(Ease.InOutSine);
+
+            Sequence seq = DOTween.Sequence();
+            seq.Append(visual.DOScale(new Vector3(1.2f, 0.8f, 1f), duration * 0.2f));
+            seq.Append(visual.DOScale(new Vector3(0.9f, 1.1f, 1f), duration * 0.6f));
+            seq.Append(visual.DOScale(Vector3.one, duration * 0.2f));
+
+            return duration;
+        }
     }
 }
