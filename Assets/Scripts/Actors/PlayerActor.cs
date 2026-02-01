c2b3ba09a951b98d653781cbb05ee37318676ba8
SaluteBEE <SaluteBEE@outlook.com>
2026-02-01 13:25:49 +0100

    add mask sprite logic

--- Assets/Scripts/Actors/PlayerActor.cs
+++ Assets/Scripts/Actors/PlayerActor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 namespace GGJ2026
@@ -11,12 +12,22 @@ namespace GGJ2026
         [SerializeField] private PlayerMind playerMind;
         [SerializeField] private SymbiosisMind symbiosisMind;
 
+        [SerializeField] List<Sprite> maskSpritesRed;
+        [SerializeField] List<Sprite> maskSpritesBlue;
+        [SerializeField] List<Sprite> maskSpritesStone;
+        [SerializeField] SpriteRenderer maskRenderer;
+
         public override FactionColor ControlColor => controlColor;
         public override FactionColor CombatColor => controlColor;
 
         public void EquipMask(FactionColor color)
         {
+            Debug.Log("Equip Mask: " + color);
+
+            
             controlColor = color;
+
+            SetCurrentMask();
 
             // 切到共生 Mind
             if (symbiosisMind != null)
@@ -50,6 +61,75 @@ namespace GGJ2026
         {
             LevelManager.Instance?.RestartCurrentLevel();
             base.Kill();
+        }
+        public void SetCurrentMask()
+        {
+            Debug.Log("Set Current Mask Sprite");
+            List<Sprite> currentSpriteList = new List<Sprite>();
+            if(controlColor == FactionColor.Red)
+                    currentSpriteList = maskSpritesRed;
+                else if(controlColor == FactionColor.Blue)
+                    currentSpriteList = maskSpritesBlue;
+                else if(controlColor == FactionColor.Green)
+                    currentSpriteList = maskSpritesStone;
+                    else
+                    return;
+            if(currentDir == MoveDir.Up)
+            {
+                maskRenderer.sprite = currentSpriteList[0];
+                transform.localScale = new Vector3(1,1,1);
+            } 
+            else if(currentDir == MoveDir.Down)
+            {
+                maskRenderer.sprite = currentSpriteList[1];
+                transform.localScale = new Vector3(1,1,1);
+            }
+            else if(currentDir == MoveDir.Left)
+            {
+                maskRenderer.sprite = currentSpriteList[2];
+                transform.localScale = new Vector3(-1,1,1);
+            }
+            else if(currentDir == MoveDir.Right)
+            {
+                maskRenderer.sprite = currentSpriteList[3];
+                transform.localScale = new Vector3(1,1,1);
+            }
+            
+        }
+        public override void  SetSpriteDirection(MoveDir d)
+        {
+            base.SetSpriteDirection(d);
+            
+            List<Sprite> currentSpriteList = new List<Sprite>();
+            if(controlColor == FactionColor.Red)
+                    currentSpriteList = maskSpritesRed;
+                else if(controlColor == FactionColor.Blue)
+                    currentSpriteList = maskSpritesBlue;
+                else if(controlColor == FactionColor.Green)
+                    currentSpriteList = maskSpritesStone;
+                    else
+                    return;
+
+            if(MoveDir.Up == d)
+            {
+                maskRenderer.sprite = currentSpriteList[0];
+                transform.localScale = new Vector3(1,1,1);
+            } 
+            else if(MoveDir.Down == d)
+            {
+                maskRenderer.sprite = currentSpriteList[1];
+                transform.localScale = new Vector3(1,1,1);
+            }
+            else if(MoveDir.Left == d)
+            {
+                maskRenderer.sprite = currentSpriteList[2];
+                transform.localScale = new Vector3(-1,1,1);
+            }
+            else if(MoveDir.Right == d)
+            {
+                maskRenderer.sprite = currentSpriteList[3];
+                transform.localScale = new Vector3(1,1,1);
+            }
         }
 
 
